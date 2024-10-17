852a547eb5f172b1776e6ece0beb11d1510ccfb1
Jan Paul <katsuragik919@gmail.com>
2024-10-17 19:03:14 +0800

    change landing page

--- src/pages/landing page/LandingPage.jsx
+++ src/pages/landing page/LandingPage.jsx
@@ -23,26 +23,6 @@ const LandingPage = () => {
                 <button className="cta-button" onClick={handleCheckOutBtn}>Check Out Now</button>
             </header>
 
-            <section className="features">
-                <h2>Features</h2>
-
-                <div className="feature">
-                    <h3>Feature 1</h3>
-                    <p>Short description of Feature 1.</p>
-                </div>
-
-                <div className="feature">
-                    <h3>Feature 2</h3>
-                    <p>Short description of Feature 2.</p>
-                </div>
-
-                <div className="feature">
-                    <h3>Feature 3</h3>
-                    <p>Short description of Feature 3.</p>
-                </div>
-
-            </section>
-            
             <footer className="landing-footer">
                 <p>© 2024 Our Company. All rights reserved.</p>
             </footer>
